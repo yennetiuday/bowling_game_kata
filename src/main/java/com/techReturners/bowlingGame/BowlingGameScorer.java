ec60690fba9b56de84002054a86c7222b4f00ebe
UDAYBHASKAR\yenne <yenneti.uday@hotmail.com>
2023-02-08 21:42:07 +0000

    Refactored BowlingGameScorer class to accomidate frames

--- src/main/java/com/techReturners/bowlingGame/BowlingGameScorer.java
+++ src/main/java/com/techReturners/bowlingGame/BowlingGameScorer.java
@@ -2,17 +2,24 @@ package com.techReturners.bowlingGame;
 
 public class BowlingGameScorer {
 	
-	private int score;
+	private int roll;
+	private int[] rolls;
 	
 	public BowlingGameScorer() {
-		score = 0;
+		rolls = new int[21];
 	}
 
 	public void roll(int pinsDown) {
-		score += pinsDown;
+		rolls[roll++] = pinsDown;
 	}
 
 	public int score() {
+		int score = 0;
+		int currentRoll = 0;
+		for(int CurrentFrame = 0; CurrentFrame<10; CurrentFrame++) {
+			score += rolls[currentRoll] + rolls[currentRoll+1];
+			currentRoll += 2;
+		}
 		return score;
 	}
 
